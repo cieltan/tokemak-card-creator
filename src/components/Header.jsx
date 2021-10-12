91a60240cb3fdb5d497cc456ddcea281bb053159
btan <btan7645@gmail.com>
2021-10-12 18:16:26 -0400

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react"
+import TokemakLogo from "../../static/assets/images/tokemak_desktop_logo.png"
 import "../styles/components/_header.scss"
+
 
 export const Header = () => {
   return <div className="headerContainer">
-    This is the header.
+    <img src={TokemakLogo} alt="Tokemak Logo" className="tokemakDesktopLogo" />
   </div>
 }
