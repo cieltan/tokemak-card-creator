766b98596379ac2816195eb4e407a09bcb97aab0
btan <btan7645@gmail.com>
2021-10-12 21:55:55 -0400

    add AudioPlayer to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react"
+import { navigate } from "gatsby-link"
 import TokemakLogo from "../../static/assets/images/tokemak_desktop_logo.png"
 import "../styles/components/_header.scss"
+import 'react-h5-audio-player/lib/styles.css'
+import AudioPlayer from 'react-h5-audio-player'
 
 
 export const Header = () => {
-  return <header className="headerContainer">
+  return <header className="headerContainer" onClick={() => {
+    navigate('/')
+  }}>
     <img src={TokemakLogo} alt="Tokemak Logo" className="tokemakDesktopLogo" />
+    <AudioPlayer className={"audioPlayer"} src="https://vgmsite.com/soundtracks/starcraft/ujczarmd/01.%20Starcraft%20Main%20Title.mp3" autoPlay={true} loop={true} volume={ .5}/>
   </header>
 }
